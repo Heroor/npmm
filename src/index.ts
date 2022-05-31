b708226ee60f576a73e151f59ef563f4d5b3ba9d
Bener <benjavan@163.com>
2022-05-31 10:53:58 +0800

    fix: suppport node12

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import path from 'path'
-import fs from 'fs/promises'
+import { access } from 'fs'
+import { resolve } from 'path'
+import { promisify } from 'util'
 import { spawn } from 'child_process'
 import { lockFileMap } from './config'
 
 const cwd = process.cwd()
 const args = process.argv.slice(2)
+const accessPromise = promisify(access)
 
 export default async function n() {
   const manager = await getManager()
@@ -16,7 +18,7 @@ export default async function n() {
 async function getManager(dfManager = 'npm') {
   for await (const [manager, lockFile] of lockFileMap) {
     try {
-      await fs.access(path.resolve(cwd, lockFile))
+      await accessPromise(resolve(cwd, lockFile))
       return manager
     } catch (_) {}
   }
